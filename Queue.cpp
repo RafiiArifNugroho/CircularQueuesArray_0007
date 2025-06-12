fd2d0af326c1e4c763626fe7e462b30f82c89078
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-06-12 11:15:32 +0700

    Add main menu interface for queue operations

--- Queue.cpp
+++ Queue.cpp
@@ -92,3 +92,20 @@ public:
         }
     }
 };
+int main()
+{
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" << endl;
+            cout << "1. implement insert operation" << endl;
+            cout << "2. implement delete operation" << endl;
+            cout << "3. Display values" << endl;
+            cout << "4. Exit" << endl;
+            cout << "Enter your choice (1-4) :";
+            cin >> ch;
+            cout << endl;
