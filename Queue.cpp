081ec10ddad6189d8c393f8049550be13fa265b4
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-06-12 11:06:56 +0700

    Wrap rear index for circular queue

--- Queue.cpp
+++ Queue.cpp
@@ -29,3 +29,12 @@ public:
             front = 0;
             rear = 0;
         }
+        else
+        {
+            if (rear == max - 1)
+                rear = 0;
+            else
+                rear = rear + 1;
+        }
+        queues_array[rear] = num;
+    }
