fc0d13e04ddb881c7535a2077950c4fa52ffa8cf
RafiiArifNugroho <rafialamcitra@gmail.com>
2025-06-12 11:03:33 +0700

    menambahkan kelas antrian dengan inisialisasi

---
+++ Queue.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Queues
+{
+    int front, rear, max = 5;
+    int queues_array[5];
+
+public:
+    Queues()
+    {
+        front = -1;
+        rear = -1;
+    }
