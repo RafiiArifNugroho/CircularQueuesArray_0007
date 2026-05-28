b574ccb9c16307a1087ac6f408cdf9aae75b7c14
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-05-28 15:33:10 +0700

    menambahkan traversal circular queue pada display

--- queue.cpp
+++ queue.cpp
@@ -102,3 +102,26 @@ public:
             }
             cout << endl;
         }
+
+        else
+        {
+            // jika REAR < FRONT, traverse dari FRONT hingga akhir array
+            while (FRONT_Position <= max - 1)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+            FRONT_Position = 0;
+
+            // traverse dari awal array hingga REAR
+            while (FRONT_Position <= REAR_Position)
+            {
+                cout << queue_array[FRONT_Position] << " ";
+                FRONT_Position++;
+            }
+
+            cout << endl;
+        }
+    }
+};
