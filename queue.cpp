d328dda2be7dce9919adc03f8d70c28e3a4ca485
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-05-28 15:40:29 +0700

    menambahkan switch case menu queue

--- queue.cpp
+++ queue.cpp
@@ -142,3 +142,17 @@ int main()
             cout << "Enter your choice (1-4): ";
             cin >> ch;
             cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+
+            case '2':
+            {
+                q.remove();
+                break;
+            }
