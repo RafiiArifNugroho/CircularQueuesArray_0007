a28ced8b6c90b4e6c274d70c7f654508058d0223
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-05-28 15:42:24 +0700

    menambahkan fitur display dan exit pada menu

--- queue.cpp
+++ queue.cpp
@@ -156,3 +156,22 @@ int main()
                 q.remove();
                 break;
             }
+
+            case '3':
+            {
+                q.display();
+                break;
+            }
+
+            case '4':
+            {
+                return 0;
+            }
+
+            default:
+            {
+                cout << "Invalid option!!!" << endl;
+                break;
+            }
+            }
+        }
