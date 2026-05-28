85780539cb8367a2e6998f6dc4409421f7f1996a
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-05-28 15:18:48 +0700

    menambahkan pengecekan queue overflow

--- queue.cpp
+++ queue.cpp
@@ -15,9 +15,16 @@ public:
         REAR = -1;
     }
 
-void insert()
+    void insert()
     {
         int num;
         cout << "Enter a number: ";
         cin >> num;
         cout << endl;
+
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue Overflow\n";
+            return;
+        }
