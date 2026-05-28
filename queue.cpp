62de95dc7f1f02b30fc89088291090b83e9cf3c4
RafiiArifNugroho <rafialamcitra@gmail.com>
2026-05-28 15:43:50 +0700

    menambahkan exception handling

--- queue.cpp
+++ queue.cpp
@@ -175,3 +175,11 @@ int main()
             }
             }
         }
+
+        catch (exception &e)
+        {
+            cout << "Check for the values entered." << endl;
+        }
+    }
+    return 0;
+}
